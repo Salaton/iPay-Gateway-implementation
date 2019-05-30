6a2d935318f981cfafa5bd3a19834806087de332
Salaton <elvisdenis056@gmail.com>
2019-05-30 08:57:37 +0300

    Generate hash and random OID in loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
-import { PaymentService } from "./payment.service";
 import * as CryptoJS from "crypto-js";
+import { Observable, throwError } from "rxjs";
+import { HttpErrorResponse } from "@angular/common/http";
 
 @Component({
 	selector: "my-app",
@@ -8,11 +9,17 @@ import * as CryptoJS from "crypto-js";
 	styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+	constructor() {
+		this.hash();
+	}
+
 	public name = "Ipay demo";
+	//Generate a random order ID each time
+	public oid = Math.floor(Math.random() * 1000000);
 
 	public data = {
 		live: "0",
-		oid: "112",
+		oid: `${this.oid}`,
 		inv: "112020102292999",
 		ttl: "990",
 		tel: "256712375678",
@@ -26,31 +33,21 @@ export class AppComponent {
 		cbk: "http://localhost:4200/",
 		cst: "1",
 		crl: "2",
-		hsh: "37ed835591e5e0a8f3f2077fd540e8daacfa6472"
+		hsh: `${this.hash}`
 	};
-	private dataStr;
-
-	constructor(private payment: PaymentService) {
-		this.dataStr = this.createDataStr;
-		// this.hash();
-	}
 
 	public secretKey = "demoCHANGED";
+	public parameters = `${this.data.live}${this.data.oid}${this.data.inv}${this.data.ttl}${this.data.tel}${this.data.eml}${this.data.vid}${this.data.curr}${this.data.p1}${this.data.p2}${this.data.p3}${this.data.p4}${this.data.cbk}${this.data.cst}${this.data.crl}`;
 
-	// public hash() {
-	// 	const hash = CryptoJS.HmacSHA1(this.data, this.secretKey).toString();
-	// 	this.data.hsh = hash;
-	// }
-
-	/* Concatenate the data into a string for hash key generation */
-	get createDataStr() {
-		return Object.entries(this.data).reduce((result, value) => {
-			return result.concat(value[1]);
-		}, "");
+	//Generate the hashkey from the provided parameters
+	public hash() {
+		const hash = CryptoJS.HmacSHA1(this.parameters, this.secretKey).toString();
+		this.data.hsh = hash;
+		console.log(this.data.hsh);
 	}
 
-	pay(dataStr: string) {
-		//return this.payment.makePayment(this.data);
+	//Concatenate the payment URL with the data provided, on clicking pay, it redirects to the payment page
+	pay() {
 		const build_query = data => {
 			let str = "";
 			Object.keys(data).forEach(param => {
@@ -61,5 +58,20 @@ export class AppComponent {
 		//console.log(build_query(this.data));
 		var path = "https://payments.ipayafrica.com/v3/ke?" + build_query(this.data);
 		window.location.href = path;
+	}
+
+	/* Error handler */
+	private handleError(error: HttpErrorResponse) {
+		if (error.error instanceof ErrorEvent) {
+			// A client-side or network error occurred. Handle it accordingly.
+			console.error("An error occurred:", error.error.message);
+		} else {
+			// The backend returned an unsuccessful response code.
+			// The response body may contain clues as to what went wrong,
+			console.error(`Backend returned code ${error.status}, ` + `body was: ${error}`);
+			console.log(error);
+		}
+		// return an observable with a user-facing error message
+		return throwError("Something bad happened; please try again later.");
 	}
 }
